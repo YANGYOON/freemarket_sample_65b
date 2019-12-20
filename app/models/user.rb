1c2311b110f311c3985889c8da618c7ae9fd55ab
YANG LIU <lynk2010@live.jp>
2019-12-20 17:44:03 +0900

    association-user-userprofile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname ,presence: true
+  validates :nickname, :email, :password, presence: true
+  has_one :user_profile
 end
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,2 +1,4 @@
 class UserProfile < ApplicationRecord
+  belongs_to :user
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :phone_number, presence: true
 end
