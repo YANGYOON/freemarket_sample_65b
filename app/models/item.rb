5086f927527684dd80d48c9d3bd80d49b9398a7f
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2019-12-24 18:28:02 +0900

    アソシエーション修正
    shippingにもaccepts_nested_attributes_forを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :images, allow_destroy: true
 
   has_one :shipping dependent: :destroy
+  accepts_nested_attributes_for :shipping, allow_destroy: true
   belongs_to :category
   belongs_to :item
 
