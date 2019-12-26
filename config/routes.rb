1c59f79429d12263d4f324a2c03f42f5e3644ad8
Milia-yuuta <57859618+Milia-yuuta@users.noreply.github.com>
2019-12-26 10:40:03 +0900

    add_8

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'user_profiles#index'
   resources :test, only: :index
   resources :user_profiles, only: :index
+  resources :creditcards
   devise_for :users
 
 end
