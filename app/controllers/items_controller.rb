0b675821105ada62ed0650cc9aff755b106cb725
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2019-12-26 14:50:31 +0900

    itemコントローラのcreateアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,10 +5,27 @@ class ItemsController < ApplicationController
     @item.images.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def buy
   end
 
   def show
   end
-  
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :category_id, images_attributes: [:image])
+  end
+
+  def set_product
+    @item = Item.find(params[:id])
+  end
 end
