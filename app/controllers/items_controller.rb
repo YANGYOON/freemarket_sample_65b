b1f41179ffebc608e84fb5588ce8f54e9e6a35c3
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2019-12-29 13:20:31 +0900

    jsの用意とビューの微調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,15 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def category_children  
+    @category_children = Category.find(params[:parent_id]).children 
+    end
+  # Ajax通信で送られてきたデータをparamsで受け取り､childrenで子を取得
+
+  def category_grandchildren
+    @category_grandchildren = Category.find(params[:parent_id]).children
+    end
+
   private
   def item_params
     params.require(:item).permit(:name, :price, :category_id, images_attributes: [:image])
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :test, only: :index
   devise_for :users
-  resources :items
+  resources :items do
+    collection do
+      get 'category_children'
+      get 'category_grandchildren'
+    end
+  end
 end
