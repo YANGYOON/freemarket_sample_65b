ff060993eedb02bda687f53a11eede1c1346dc1b
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2019-12-29 18:23:01 +0900

    アソシエーションの修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,5 @@ class Category < ApplicationRecord
   has_many :items
   has_ancestry
 
-  has_many :sizes, through: :category_sizes
-  has_many :category_sizes
+  has_many :sizes
 end
--- app/models/size.rb
+++ app/models/size.rb
@@ -1,4 +1,3 @@
 class Size < ApplicationRecord
-  has_many :categorys, through: :category_sizes
-  has_many :category_sizes
+  belong_to :category
 end
