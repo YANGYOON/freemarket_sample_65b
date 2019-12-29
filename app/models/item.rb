703a839f4ae42348cb63b98804f5b2cad687340f
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2019-12-29 17:56:07 +0900

    sizeテーブルとcategory,sizeの中間テーブルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,8 @@ class Item < ApplicationRecord
   has_one :shipping, dependent: :destroy
   accepts_nested_attributes_for :shipping, allow_destroy: true
   belongs_to :category
-  # belongs_to :brand, optional: true
+  belongs_to :brand, optional: true
+  belongs_to :size, optional: true
 
   # belongs_to :seller, class_name: "User"
   # belongs_to :buyer, class_name: "User", optional: true
--- db/migrate/20191230045832_add_foreign_key_to_item.rb
+++ db/migrate/20191230045832_add_foreign_key_to_item.rb
@@ -2,5 +2,6 @@ class AddForeignKeyToItem < ActiveRecord::Migration[5.2]
   def change
     add_reference :items, :category, foreign_key: true, null: false
     add_reference :items, :brand, foreign_key: true
+    add_reference :items, :size, foreign_key: true
   end
 end
