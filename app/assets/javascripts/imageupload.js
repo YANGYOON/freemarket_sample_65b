93dcff990e59952c088b86162e032652943fcb30
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2020-01-01 23:16:39 +0900

    画像のプレビュー完成

--- app/assets/javascripts/imageupload.js
+++ app/assets/javascripts/imageupload.js
@@ -40,7 +40,8 @@ $(document).on('turbolinks:load', ()=> {
     } else {  // 新規画像追加の処理
       $('.contents__main__image__box__previews').append(buildImg(targetIndex, blobUrl));
       // fileIndexの先頭の数字を使ってinputを作る
-      $('.contents__main__image__box__uploader').append(buildFileField(fileIndex[0]));
+      $('.contents__main__image__box__uploader__label').prepend(buildFileField(fileIndex[0]));
+      $(this).css({'display':'none'});
       fileIndex.shift();
       // 末尾の数に1足した数を追加する
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
@@ -68,13 +69,13 @@ $(document).on('turbolinks:load', ()=> {
       switch (img_count){
         case 0:
           $('.contents__main__image__box__previews').css({
-            'display':'none',
-          });;
+            'display':'none'
+          });
           $('.contents__main__image__box__uploader').css({
             'width':'500%',
             'grid-column-start':'1',
             'grid-row-start':'1'
-          });;
+          });
           break;
         case 1:
           $('.contents__main__image__box__previews').css({
@@ -82,145 +83,145 @@ $(document).on('turbolinks:load', ()=> {
             'grid-template-rows':'repeat(1, 165px)',
             'grid-template-columns':'repeat(1, 120px)',
             'width':'120px'
-          });;
+          });
           $('.contents__main__image__box__uploader').css({
             'width':'400%',
             'grid-column-start':'2',
             'grid-row-start':'1'
-          });;
+          });
           break;
         case 2:
           $('.contents__main__image__box__previews').css({
             'display':'grid',
             'grid-template-rows':'repeat(1, 165px)',
             'grid-template-columns':'repeat(2, 120px)',
-          });;
+          });
           $('.contents__main__image__box__uploader').css({
             'width':'300%',
             'grid-column-start':'3',
             'grid-row-start':'1'
-          });;
+          });
           break;
         case 3:
           $('.contents__main__image__box__previews').css({
             'display':'grid',
             'grid-template-rows':'repeat(1, 165px)',
             'grid-template-columns':'repeat(3, 120px)',
-          });;
+          });
           $('.contents__main__image__box__uploader').css({
             'width':'200%',
             'grid-column-start':'4',
             'grid-row-start':'1'
-          });;
+          });
           break;
         case 4:
           $('.contents__main__image__box__previews').css({
             'display':'grid',
             'grid-template-rows':'repeat(1, 165px)',
             'grid-template-columns':'repeat(4, 120px)',
-          });;
+          });
           $('.contents__main__image__box__uploader').css({
             'width':'120px',
             'grid-column-start':'5',
             'grid-row-start':'1'
-          });;
+          });
           $('.contents__main__image').css({
             'height':'190px'
-          });;
+          });
           break;
         case 5:
           $('.contents__main__image__box__previews').css({
             'display':'grid',
             'grid-template-rows':'repeat(1, 165px)',
             'grid-template-columns':'repeat(5, 120px)',
-          });;
+          });
           $('.contents__main__image__box__uploader').css({
             'grid-template-rows':'repeat(2, 169px)',
             'width':'500%',
             'grid-column-start':'1',
             'grid-row-start':'2'
-          });;
+          });
           $('.contents__main__image').css({
             'height':'360px'
-          });;
+          });
           break;
         case 6:
           $('.contents__main__image__box__previews').css({
             'display':'grid',
             'grid-template-rows':'repeat(2, 165px)',
             'grid-template-columns':'repeat(5, 120px)',
-          });;
+          });
           $('.contents__main__image__box__uploader').css({
             'grid-template-rows':'repeat(2, 169px)',
             'width':'400%',
             'grid-column-start':'2',
             'grid-row-start':'2'
-          });;
+          });
           $('.contents__main__image').css({
             'height':'360px'
-          });;
+          });
           break;
         case 7:
           $('.contents__main__image__box__previews').css({
             'display':'grid',
             'grid-template-rows':'repeat(2, 165px)',
             'grid-template-columns':'repeat(5, 120px)',
-          });;
+          });
           $('.contents__main__image__box__uploader').css({
             'grid-template-rows':'repeat(2, 169px)',
             'width':'300%',
             'grid-column-start':'3',
             'grid-row-start':'2'
-          });;
+          });
           $('.contents__main__image').css({
             'height':'360px'
-          });;
+          });
           break;
         case 8:
           $('.contents__main__image__box__previews').css({
             'display':'grid',
             'grid-template-rows':'repeat(2, 165px)',
             'grid-template-columns':'repeat(5, 120px)',
-          });;
+          });
           $('.contents__main__image__box__uploader').css({
             'grid-template-rows':'repeat(2, 169px)',
             'width':'200%',
             'grid-column-start':'4',
             'grid-row-start':'2'
-          });;
+          });
           $('.contents__main__image').css({
             'height':'360px'
-          });;
+          });
           break;
         case 9:
           $('.contents__main__image__box__previews').css({
             'display':'grid',
             'grid-template-rows':'repeat(2, 165px)',
             'grid-template-columns':'repeat(5, 120px)',
-          });;
+          });
           $('.contents__main__image__box__uploader').css({
             'display':'grid',
             'grid-template-rows':'repeat(2, 169px)',
             'width':'100%',
             'grid-column-start':'5',
             'grid-row-start':'2'
-          });;
+          });
           $('.contents__main__image').css({
             'height':'360px'
-          });;
+          });
           break;
         case 10:
           $('.contents__main__image__box__previews').css({
             'display':'grid',
             'grid-template-rows':'repeat(2, 165px)',
             'grid-template-columns':'repeat(5, 120px)',
-          });;
+          });
           $('.contents__main__image__box__uploader').css({
             'display':'none',
-          });;
+          });
           $('.contents__main__image').css({
             'height':'360px'
-          });;
+          });
           break;
       }
     });
