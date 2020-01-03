b9834ca2cadb058899d3da48dc5017875efc0140
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2020-01-03 13:50:40 +0900

    単体テストの追加

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 describe ItemsController, type: :controller do
-
-    describe 'GET #new' do
-      it "renders the :new template" do
-        get :new
-        expect(response).to render_template :new
-      end
+  describe 'GET #index' do
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
     end
+  end
 
+  describe 'GET #new' do
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     price          {1000}
     condition      {"新品、未使用"}
     category_id    {1}
+    created_at { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
     shipping
   end
 end
