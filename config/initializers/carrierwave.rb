c84ea1e350ef8d33583be8cd292c86cc2f81ba9a
Milia-yuuta <57859618+Milia-yuuta@users.noreply.github.com>
2020-01-06 17:42:44 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1' # 東京はap-northeast-1
     }
     config.fog_directory  = 'mercari65b' #S3のバケット名
-    config.asset_host = 'https://s3-your_region.amazonaws.com/your_bucket_name'
+    config.asset_host = 'https://s3-your_region.amazonaws.com/mercari65b'
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
