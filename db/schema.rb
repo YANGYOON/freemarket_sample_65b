0828fff5edcfb3493acaa9e89652284535755b55
tksagosago <senkanyamayama@gmail.com>
2020-01-06 14:41:09 +0900

    コンフリクト修正3

--- db/schema.rb
+++ db/schema.rb
@@ -32,13 +32,6 @@ ActiveRecord::Schema.define(version: 2020_01_05_052700) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "ancestry"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
     t.bigint "item_id", null: false
