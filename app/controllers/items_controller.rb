f795e377abe3210c567aeaa8809fbaf06a8522fb
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2020-01-06 11:54:31 +0900

    !の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -63,7 +63,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy!
+    if @item.destroy
       redirect_to root_path
     else
       redirect_to action: 'edit'
