b6c852805a3623e165993d31a57071d948de215f
tksagosago <senkanyamayama@gmail.com>
2020-01-06 14:35:21 +0900

    コンフリクト修正

--- db/schema.rb
+++ db/schema.rb
@@ -39,15 +39,6 @@ ActiveRecord::Schema.define(version: 2019_12_30_045832) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "creditcards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_creditcards_on_user_id"
-  end
-
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
     t.bigint "item_id", null: false
@@ -92,11 +83,13 @@ ActiveRecord::Schema.define(version: 2019_12_30_045832) do
     t.index ["item_id"], name: "index_shippings_on_item_id"
   end
 
-  create_table "sizes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "classification"
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
