ff058d1b7cd6363acd2616bb56423ea83d40b879
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2020-01-07 17:28:09 +0900

    カテゴリィプルダウンの作成(途中)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,10 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.order("id ASC").limit(2)
+    @categories = Category.order("id ASC").limit(13)
 
   end
 
   def show
-  
+    @selected_category = Category.find(params[:id])
   end
 end
