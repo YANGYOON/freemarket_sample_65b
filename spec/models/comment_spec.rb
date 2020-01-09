e39c7499d9be7e1d4de8999498d9196e533425bb
tksagosago <senkanyamayama@gmail.com>
2020-01-09 11:46:34 +0900

    コメントモデル単体テスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+
+    it "is invalid without a text" do
+      comment = build(:comment, text: "")
+      comment.valid?
+      expect(comment.errors[:text]).to include("can't be blank")
+    end
+
+    it "is invalid without a user_id" do
+      comment = build(:comment, user_id: "")
+      comment.valid?
+      expect(comment.errors[:user_id]).to include("can't be blank")
+    end
+
+    it "is invalid without a item_id" do
+      comment = build(:comment, item_id: "")
+      comment.valid?
+      expect(comment.errors[:item_id]).to include("can't be blank")
+    end
+
+  end
 end
