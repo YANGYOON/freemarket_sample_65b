cedf952111d5937bebe826ac804958dbe480c128
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2020-01-12 20:44:35 +0900

    トレンド機能(完成)

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,6 +1,7 @@
 class GraphsController < ApplicationController
   def index
-    # @trend_categories = Item.joins(:category).group("categories.name").order('count_all DESC').count
-    @trend_categories = Item.group(:category_id).count
+    @trend_categories_name = Item.joins(:category).group("categories.name").order('count_all DESC').limit(5).count.to_a
+    @trend_categories_id = Item.group(:category_id).order('count_all DESC').limit(5).count.to_a
+    @trend_brands_name = Item.joins(:brand).group("brands.name").order('count_all DESC').limit(5).count.to_a
   end
 end
