80ddbf1ffeb1b64d9dc8efb48be2ee392463e8bd
Takeishui_yuya <yuya_dayo_7@icloud.com>
2020-01-13 07:40:31 +0900

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+ 
+         
   has_one :address, dependent: :destroy
   has_one :phone_number, dependent: :destroy
   has_one :creditcard, dependent: :destroy
