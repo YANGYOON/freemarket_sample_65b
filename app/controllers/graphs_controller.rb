bea9e54d88d2bd62e93efe5e247689aabc28d64e
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2020-01-12 17:12:52 +0900

    カテゴリィのデータの取得と表示
    コントローラで@trend_categoriesとしてtop5のカテゴリィを取得

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,2 +1,6 @@
 class GraphsController < ApplicationController
+  def index
+    # @trend_categories = Item.joins(:category).group("categories.name").order('count_all DESC').count
+    @trend_categories = Item.group(:category_id).count
+  end
 end
