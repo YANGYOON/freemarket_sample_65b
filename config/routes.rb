9b8a24169c0f975093c68be52e1ee01dd478d3ba
Takeishui_yuya <yuya_dayo_7@icloud.com>
2020-01-13 10:53:40 +0900

    conclift3

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,36 @@
-Rails.application.routes.draw do
-
-  devise_for :users, controllers: {
+devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations',
   }
   devise_scope :user do
+    get 'users', to: 'users/registrations#index'
     get 'phone_numbers', to: 'users/registrations#new_phone_number'
+    get 'signup_creditcards', to: 'users/registrations#signup_creditcard'
     get 'addresses', to: 'users/registrations#new_address'
-    post 'addresses', to: 'users/registrations#create_address'
+    post 'signup_create', to: 'users/registrations#signup_create'
   end
   root to: 'items#index'
-  resources :items
+  resources :items do
+    collection do
+      get 'category_children'
+      get 'category_grandchildren'
+      get 'set_sizes'
+      get 'cal_profit'
+    end
+    collection do
+      get 'search'
+    end
+    resources :purchase do
+      collection do
+        get 'buy'
+        post 'pay'
+        get 'done'
+      end
+    end
+    resources :comments, only: :create
+  end
   resources :test, only: :index
   resources :user_profiles, only: :index
+  resources :creditcards, only: [:create, :new, :index, :show, :destroy]
+  resources :users
+  resources :categories, only: [:index, :show]
