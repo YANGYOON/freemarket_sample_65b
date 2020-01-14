f433c65f2d30cd320c997bdc9ecb560a90fb41b2
tksagosago <senkanyamayama@gmail.com>
2020-01-14 19:32:13 +0900

    コンフリクト解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,8 +20,8 @@ class Item < ApplicationRecord
     Item.where('name LIKE(?)', "%#{search}%")
   end
 
-  # belongs_to :seller, class_name: "User"
-  # belongs_to :buyer, class_name: "User", optional: true
+  belongs_to :seller, class_name: "User"
+  belongs_to :buyer, class_name: "User", optional: true
 
   validates :name, presence: true
   validates :state, presence: true
