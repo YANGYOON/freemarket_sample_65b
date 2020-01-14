332e9c31b49ae8c3dc912698046f326e79c4a167
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2020-01-14 12:01:34 +0900

    ヘルパーメソッドの作成
    renderを読み込むためのヘルパーメソッドを作成

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,13 @@
 module ItemsHelper
+  def search_new_items_category(root_category_id)
+    root_category = Category.find(root_category_id)
+    grandchildren_ids = root_category.indirect_ids
+    new_items_category = Item.where(category_id: grandchildren_ids).order('created_at DESC').limit(20)
+    if new_items_category[0] != nil
+      sell_items_list = render partial: 'sell_items_list', locals: { root_category: root_category, items: new_items_category }
+    else 
+      sell_items_list = nil
+    end
+    return sell_items_list
+  end
 end
