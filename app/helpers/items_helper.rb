70e77e0e7acb7421b8cbea8f7044d8436572cdcd
Clora-Monet <pulcherrima@guitar.ocn.ne.jp>
2020-01-15 14:05:32 +0900

    itemのヘルパーメソッド編集
    トップページに売り切れの商品が引っかからないようにした

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,7 +2,7 @@ module ItemsHelper
   def search_new_items_category(root_category_id)
     root_category = Category.find(root_category_id)
     grandchildren_ids = root_category.indirect_ids
-    new_items_category = Item.where(category_id: grandchildren_ids).order('created_at DESC').limit(20)
+    new_items_category = Item.where(level: 0).where(category_id: grandchildren_ids).order('created_at DESC').limit(20)
     if new_items_category[0] != nil
       sell_items_list = render partial: 'sell_items_list_top', locals: { root_category: root_category, items: new_items_category }
     else 
